8a729488123175ef1ba22f87fe0cdda7d85cced2
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-12-12 15:14:08 +0300

    add component UserTitle

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,15 +1,26 @@
 import styles from "./UserCard.module.scss";
 import { LocalGithubUser } from "../../types";
 import { UserStat } from "../UserStat";
+import { UserTitle } from "../UserTitle";
 
-type UserCardProps = Pick<LocalGithubUser, "repos" | "followers" | "following">;
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface UserCardProps extends LocalGithubUser {}
 
-export const UserCard = (props: UserCardProps) => (
- <div className={styles.userCard}>
-  <UserStat
-   repos={props.repos}
-   followers={props.followers}
-   following={props.following}
-  />
- </div>
-);
+
+export const UserCard = (props: UserCardProps) => {
+ return (
+  <div className={styles.userCard}>
+   <img src={props.avatar} alt={props.login} className={styles.avatar}/>
+   <UserTitle 
+   created={props.created}
+   login={props.login}
+   name={props.name}
+   />
+   <UserStat
+    repos={props.repos}
+    followers={props.followers}
+    following={props.following}
+   />
+  </div>
+ );
+};
